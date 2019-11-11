981a0c1e739810b72dd1e76a9f9abbe5564846f1
caleb1000 <caleb.steinmetz@yahoo.com>
2019-11-11 16:02:32 -0500

    irelis helped

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/home.style.css";
+//import "../style/home.style.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div>
+
+    //<div>
+
+      /*<div className="home-container">
+      <img src={ require('../assets/test.jpg') } />
         <p>Home</p>
         <p>This is the Landing Page</p>
-        <Link to="/MainMenu">
-          <button className="home-button">Click to go to Main Menu</button>
+        
+      </div>*/
+//</div>
+
+<div>
+  
+
+
+    <div class = "home-image">
+      <img src={ require('../assets/anewlifelinenew.jpeg') } />
+        <Link to="/users/login">
+          <button className="home-button">Log In</button>
         </Link>
-      </div>
+        <Link to="/users/register">
+          <button className="home-button2">Sign Up</button>
+        </Link>
     </div>
+</div>
+
   );
 };
 
