9d4aae3dd067dfcc8ce0f44daf9f68434af8a063
paolagabriellas <54489794+paolagabriellas@users.noreply.github.com>
2019-11-11 19:23:16 -0500

    Revert "About page"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/home.style.css";
-//import "../style/home.style.css";
 
 const Home = () => {
   return (
-
-    //<div>
-
-      /*<div className="home-container">
-      <img src={ require('../assets/test.jpg') } />
+    <div className="home-container">
+      <div>
         <p>Home</p>
         <p>This is the Landing Page</p>
-        
-      </div>*/
-//</div>
-
-<div>
-  
-
-
-    <div class = "home-image">
-      <img src={ require('../assets/anewlifelinenew.jpeg') } />
-        <Link to="/users/login">
-          <button className="home-button">Log In</button>
+        <Link to="/MainMenu">
+          <button className="home-button">Click to go to Main Menu</button>
         </Link>
-        <Link to="/users/register">
-          <button className="home-button2">Sign Up</button>
-        </Link>
+      </div>
     </div>
-</div>
-
   );
 };
 
