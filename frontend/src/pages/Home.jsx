96bf8d92371947183157da67320e7bdacf824518
izzyc323 <castroi@ufl.edu>
2019-11-24 16:22:26 -0500

    Home page elements all added, still need to fix width of screen issue

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,18 +24,33 @@ const Home = () => {
             </div>
             <div className="landing-tile-area-container">
                 <div className="landing-tile-container1">
+                    <img src={require('../assets/calendar.png')} />
                     <div className="landing-tile-title">Forum</div>
                     <div className="landing-tile-text">A place to connect with other people...</div>
                 </div>
                 <div className="landing-tile-container2">
+                    <img src={require('../assets/speech-bubbles.png')} />
                     <div className="landing-tile-title">Calendar</div>
                     <div className="landing-tile-text">A place to connect with other people...</div>
                 </div>
                 <div className="landing-tile-container3">
+                    <img src={require('../assets/tablet.png')} />
                     <div className="landing-tile-title">Connect</div>
                     <div className="landing-tile-text">A place to connect with other people...</div>
                 </div>
             </div>
+            <div className="landing-tile-area-container">
+                <div className="about-image-container">
+                    <img src={require('../assets/default-profile.png')} />
+                </div>
+                <div className="about-tile-container">
+                    <div className="landing-tile-title">Steven Revels</div>
+                    <div className="landing-tile-title2">Founder</div>
+                    <p></p>
+                    <div className="landing-tile-text">About Steven...</div>
+                </div>
+            </div>
+            <div className="home-footer"></div>
         </div>
     );
 };
